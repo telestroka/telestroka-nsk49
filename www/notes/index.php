1f3b7d5f3a6511041768652eadb8689ab857d3d3
Mirantus <the-ms@yandex.com>
2014-02-18 16:45:29 +0700

    update design

--- www/notes/index.php
+++ www/notes/index.php
@@ -5,7 +5,17 @@ require('./init.php');
 if ($site->getUserAccess() > 3) {
 	$notes = $site->db->query('SELECT * FROM notes ORDER BY date DESC;')->fetchAll(PDO::FETCH_ASSOC);
 } else {
-	$notes = $site->db->query('SELECT * FROM notes ORDER BY date DESC LIMIT 0,3;')->fetchAll(PDO::FETCH_ASSOC);
+	$per_page = 11;
+	$first = $site->getPageItemsFirst($per_page);
+	$notes = $site->db->query(
+		'SELECT * FROM notes ORDER BY date LIMIT ' . $first . ',' . $per_page . ';'
+	)->fetchAll(PDO::FETCH_ASSOC);
+	$count = $site->db->query('SELECT COUNT(*) FROM notes')->fetchColumn();
+	$pager = $site->getPager($per_page, $count);
+
+	$currentPage = $site->getParam('page');
+	if ($currentPage == '') $currentPage = 1;
+	$countPages = ceil($count / $per_page);
 }
 
 include($site->layoutPath . '/default.phtml');
