16d70882ad5d5547c77153edcf34b2c1b1891e28
Mirantus <the-ms@yandex.com>
2014-02-18 16:53:46 +0700

    change reordering for notes

--- www/notes/index.php
+++ www/notes/index.php
@@ -8,7 +8,7 @@ if ($site->getUserAccess() > 3) {
 	$per_page = 11;
 	$first = $site->getPageItemsFirst($per_page);
 	$notes = $site->db->query(
-		'SELECT * FROM notes ORDER BY date LIMIT ' . $first . ',' . $per_page . ';'
+		'SELECT * FROM notes ORDER BY date DESC LIMIT ' . $first . ',' . $per_page . ';'
 	)->fetchAll(PDO::FETCH_ASSOC);
 	$count = $site->db->query('SELECT COUNT(*) FROM notes')->fetchColumn();
 	$pager = $site->getPager($per_page, $count);
